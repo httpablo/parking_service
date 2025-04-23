9bc56294b9b6b8184a3e733a99b81d3d69654dfe
httpablo <pablobrith@gmail.com>
2025-04-23 09:50:31 -0300

    vehicle and parking models api

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('api/v1/', include('customers.urls')),
+    path('api/v1/', include('parking.urls')),
+    path('api/v1/', include('vehicles.urls')),
 
     path('admin/', admin.site.urls),
 ]
--- parking/views.py
+++ parking/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from parking.models import ParkingSpot, ParkingRecord
+from parking.serializers import ParkingSpotSerializer, ParkingRecordSerializer
 
-# Create your views here.
+
+class ParkingSpotViewSet(viewsets.ModelViewSet):
+    queryset = ParkingSpot.objects.all()
+    serializer_class = ParkingSpotSerializer
+
+
+class ParkingRecordViewSet(viewsets.ModelViewSet):
+    queryset = ParkingRecord.objects.all()
+    serializer_class = ParkingRecordSerializer
